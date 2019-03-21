0830012d29d2360d2985d89810be72e4e0a4f801
austin <austin.blayney22@gmail.com>
2019-03-20 20:11:25 -0600

    commit

--- src/components/Clock.vue
+++ src/components/Clock.vue
@@ -1,21 +1,40 @@
 <template>
   <div class="clock">
     <div id="clock">
-      <p class="date">{{ date }}</p>
-      <p class="time">{{ time }}</p>
+      <p class="date">{{ date() }}</p>
+      <p class="time">{{ time() }}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-   name: "clock",
-   props: [],
-   data() {
-      return {}
-   },
-   computed: {},
-   methods: {},
-   components: {}
+  name: "clock",
+  props: [],
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    date() {
+      let d = new Date();
+      return d.getMonth() + 1 + "-" + d.getDate() + "-" + d.getFullYear();
+    },
+    time() {
+      let t = new Date();
+      return t.getHours() + ":" + t.getMinutes() + ":" + t.getSeconds();
+    }
+  },
+  components: {}
+};
+</script>
+
+<style>
+.date {
+  color: antiquewhite;
 }
-   </script>
+
+.time {
+  color: antiquewhite;
+}
+</style>
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,7 @@
    <div class="dashboard container-fluid">
       <quote />
       <weather />
+      <clock />
       <!-- Among other things, you'll need a router-link somewhere in this template that will change the path to todos -->
       <router-link :to="{ name: 'todos' }">
          Todos
@@ -13,6 +14,7 @@
    //this imports some code within this file that can know be used within the file
    import Quote from '@/components/Quote.vue'
    import Weather from '@/components/Weather.vue'
+   import Clock from '@/components/Clock.vue'
    //what else do you want to import?
    export default {
       name: "dashboard",
@@ -24,7 +26,8 @@
       methods: {},
       components: {//here we register any of the imported code that we want to utilize as components within our template
          Quote,
-         Weather
+         Weather,
+         Clock
       }
    }
 </script>
